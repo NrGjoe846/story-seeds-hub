f837fcdd565d358cd1bfe0c1c7ffef83a64c7760
Nehemiah Nesanathan <144255177+NrGjoe846@users.noreply.github.com>
2025-11-22 17:38:47 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "@/contexts/AuthContext";
+import { StoriesProvider } from "@/contexts/StoriesContext";
 import Home from "./pages/Home";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
@@ -13,32 +15,44 @@ import Leaderboard from "./pages/Leaderboard";
 import Competitions from "./pages/Competitions";
 import FAQ from "./pages/FAQ";
 import Contact from "./pages/Contact";
+import Profile from "./pages/Profile";
+import UploadStory from "./pages/UploadStory";
+import StoryDetails from "./pages/StoryDetails";
+import CompetitionDetail from "./pages/CompetitionDetail";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/gallery" element={<Gallery />} />
-          <Route path="/voting" element={<Voting />} />
-          <Route path="/leaderboard" element={<Leaderboard />} />
-          <Route path="/competitions" element={<Competitions />} />
-          <Route path="/faq" element={<FAQ />} />
-          <Route path="/contact" element={<Contact />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
+    <AuthProvider>
+      <StoriesProvider>
+        <TooltipProvider>
+          <Toaster />
+          <Sonner />
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/gallery" element={<Gallery />} />
+              <Route path="/voting" element={<Voting />} />
+              <Route path="/leaderboard" element={<Leaderboard />} />
+              <Route path="/competitions" element={<Competitions />} />
+              <Route path="/competition/:id" element={<CompetitionDetail />} />
+              <Route path="/faq" element={<FAQ />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/upload" element={<UploadStory />} />
+              <Route path="/story/:id" element={<StoryDetails />} />
+              {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </BrowserRouter>
+        </TooltipProvider>
+      </StoriesProvider>
+    </AuthProvider>
   </QueryClientProvider>
 );
 
